b02dd05312ea79aa44d6b316aab66c8d5f6edc1d
3703599 <fleandrei@yahoo.com>
2019-05-03 14:05:04 +0200

    avancement

--- Script.R
+++ Script.R
@@ -1,6 +1,12 @@
 donne=read.table("adult.data", header=TRUE)
+donne=donne[,-c(3)]
+donne=donne[apply(donne[,c(1,2)],1,function (x) !is.na(x)),]
 head(donne)
 summary(donne)
 str(donne)
 #pairs(donne)
 
+donne$age.<-as.numeric(donne$age.)
+donne$capital.loss.<-as.numeric(donne$capital.loss.)
+donne$capital.gain.<-as.numeric(donne$capital.gain.)
+donne$hoursperweek.<-as.numeric(donne$hoursperweek.)
